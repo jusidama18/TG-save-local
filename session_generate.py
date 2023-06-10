c6196d416b22a87a000bcf5ba7f51f5fa3f4c831
jusidama18 <jusidama18@gmail.com>
2023-06-10 10:52:21 +0700

    Update

---
+++ session_generate.py
@@ -0,0 +1,14 @@
+try:
+    from pyrogram import Client
+except Exception as e:
+    print(e)
+    print('\nInstall pyrogram: pip3 install pyrogram')
+    exit(1)
+
+print('Required pyrogram V2 or greater.')
+API_KEY = int(input("Enter API KEY: "))
+API_HASH = input("Enter API HASH: ")
+with Client(name='USS', api_id=API_KEY, api_hash=API_HASH, in_memory=True) as app:
+    string = app.export_session_string()
+    app.send_message("me", f"**SESSION_STRING**\n\n`{string}`")
+    print(string)
