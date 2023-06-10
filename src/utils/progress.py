ff367b00b9d752799baea07e3dd34738189dfbe4
jusidama18 <jusidama18@gmail.com>
2023-06-10 12:21:50 +0700

    Update

--- src/utils/progress.py
+++ src/utils/progress.py
@@ -72,7 +72,7 @@ class Progress:
             )
             _index = ProgressTask[self.chatID].index(self.mID)
             ProgressTask[self.chatID].pop(_index)
-            await self._client.stop_transmission()
+            await self.client.stop_transmission()
 
 
         diff = now - self.start
